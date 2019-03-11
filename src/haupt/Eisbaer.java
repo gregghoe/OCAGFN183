59c5a254f6a90c5b6e829b16fe46c9d11323781a
r.schibitzki <r.schibitzki@10.100.4.27>
2019-03-11 16:13:06 +0100

    frgzug FUUU
    Change-Id: I7e9fdfc2fbd3cf4686be81fec6219a7b4e34f0ba
    Signed-off-by: r.schibitzki <r.schibitzki@10.100.4.27>

---
+++ src/haupt/Eisbaer.java
@@ -0,0 +1,61 @@
+package haupt;
+
+public class Eisbaer extends Tier implements IRaubtier
+{
+	
+	@Override
+	public boolean fresse(IOpfertier opfer) 
+	{
+		if (opfer.getLeben() > 0)
+		{
+			
+			System.out.println(this.getName()+ "Mampft");
+			opfer.setLeben(opfer.getLeben() -1);		
+			return true;
+			}
+		else
+		{
+			System.out.println(this.getName()+ " sagt: Nope nix zu fressen da");
+			return false;
+		}
+	}
+
+	@Override
+	public String kampfschrei() 
+	{
+		String x = "RAAAAAORRR";
+		return x;
+	}
+	
+	@Override
+	public String getName() 
+	{
+		return "Robin";
+	}
+
+	@Override
+	public int getLeben() 
+	{
+		return 666;
+	}
+	
+	@Override
+	public boolean lebendig() 
+	{
+		if (this.getLeben()> 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	
+	@Override
+	public void aua() 
+	{
+		System.out.println("Au.... =(");
+	}
+}
