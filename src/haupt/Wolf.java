5a08c940230042c2276f507cf70db3b8fafbb5bf
r.thiel <r.thiel@10.100.6.15>
2019-03-11 15:34:23 +0100

    lala

---
+++ src/haupt/Wolf.java
@@ -0,0 +1,34 @@
+package haupt;
+
+public class Wolf extends Tier
+{
+
+	@Override
+	public String getName()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLeben()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean lebendig()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void aua()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
