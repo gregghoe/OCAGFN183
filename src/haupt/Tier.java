8209ae71af19de28588a7b099756176e35a2c0f7
karlyankovic <karlyankovic@10.2.11.11>
2019-03-13 11:03:34 +0100

    tier

--- src/haupt/Tier.java
+++ src/haupt/Tier.java
@@ -2,14 +2,11 @@ package haupt;
 
 public abstract class Tier
 {
-<<<<<<< HEAD
+
 	protected  int leben;
-=======
 	String name;
-	int leben;
 	int staerke;
 	
->>>>>>> branch 'master' of https://github.com/rdkrl/OCAGFN183.git
 	public abstract String getName();
 	public abstract int getLeben();
 	public abstract void setLeben(int i);
